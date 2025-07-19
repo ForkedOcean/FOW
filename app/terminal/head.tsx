b3a4d6cb92673434c12774c267f49a341fbbee3c
ForkedM1nd <ForkedM1nd@outlook.com>
2025-07-19 03:19:50 +0300

    18th commit V0.0.0

---
+++ app/terminal/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+    return (
+        <>
+            <title>Terminal | ForkedOcean</title>
+            <meta name="robots" content="noindex, nofollow" />
+        </>
+    );
+}
